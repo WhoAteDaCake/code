2de7026c8c2461892c2c31e7055db78ec9324b9d
WhoAteDaCake <doitsimplefy@gmail.com>
2019-03-12 19:19:39 +0000

    update colors

--- gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/Wall.cpp
+++ gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/Wall.cpp
@@ -1,6 +1,8 @@
 #include "Wall.h"
 
-#define _TESSELATION_RES 100.0f;
+GLfloat ambient[] = {0.5f, 0.5f, 0.5f, 1.0f};
+GLfloat diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat specular[] = {0.0f, 0.0f, 0.0f, 1.0f};
 
 GLfloat mp(GLfloat p1[], GLfloat p2[], int i)
 {
@@ -48,23 +50,8 @@ void subdivide(int level, GLfloat p1[], GLfloat p2[], GLfloat p3[])
 
 Wall::Wall()
 {
-  // Grey
-  static GLfloat mat_ambient[] =
-      {
-          0.5f, 0.5f, 0.5f, 1.0f};
-  _mat_ambient = mat_ambient;
-  // White
-  static GLfloat mat_diffuse[] =
-      {
-          1.0f, 1.0f, 1.0f, 1.0f};
-  _mat_diffuse = mat_diffuse;
-  // No specular (black)
-  static GLfloat mat_specular[] =
-      {
-          0.0f, 0.0f, 0.0f, 1.0f};
-  _mat_specular = mat_specular;
+
   // Matte (not shiny)
-  _mat_shininess = 1.0f;
 }
 
 void Wall::Display()
@@ -78,10 +65,10 @@ void Wall::Display()
   glDisable(GL_COLOR_MATERIAL);
   glEnable(GL_LIGHTING);
 
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, _mat_ambient);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, _mat_diffuse);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, _mat_specular);
-  glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, _mat_shininess);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+  glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 1.f);
 
   float size = 0.5f;
   float z = 0.f;
@@ -95,41 +82,6 @@ void Wall::Display()
   int level = 5;
   subdivide(level, p[0], p[1], p[2]);
   subdivide(level, p[0], p[2], p[3]);
-  // glBegin(GL_TRIANGLES);
-  // glNormal3f(0.f, 0.f, 1.0f);
-
-  // glVertex3fv(points[0]);
-  // glVertex3fv(points[1]);
-  // glVertex3fv(points[2]);
-
-  // glVertex3fv(points[0]);
-  // glVertex3fv(points[2]);
-  // glVertex3fv(points[3]);
-
-  // glEnd();
-
-  // GLfloat _element_size = 1.0f / _TESSELATION_RES;
-
-  // for (GLfloat i = -0.5f; i < 0.5f; i += _element_size)
-  // {
-  //   for (GLfloat j = -0.5f; j < 0.5f; j += _element_size)
-  //   {
-  //     glBegin(GL_TRIANGLES);
-  //     {
-  //       glNormal3f(0.0f, 0.0f, 1.0f);
-
-  //       glVertex3f(i, j, 0.0f);
-  //       glVertex3f(i + _element_size, j, 0.0f);
-  //       glVertex3f(i, j + _element_size, 0.0f);
-
-  //       glVertex3f(i + _element_size, j, 0.0f);
-  //       glVertex3f(i + _element_size, j + _element_size, 0.0f);
-  //       glVertex3f(i, j + _element_size, 0.0f);
-  //     }
-  //     glEnd();
-  //   }
-  // }
-
   glPopAttrib();
   glPopMatrix();
 }
