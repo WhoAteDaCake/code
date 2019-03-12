ae9fc5cd858e3946472ba4076a64d01dfff693aa
WhoAteDaCake <doitsimplefy@gmail.com>
2019-03-12 19:55:26 +0000

    clean up

--- gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/Light.h
+++ gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/Light.h
@@ -8,17 +8,11 @@ class Light : public DisplayableObject, public Animation
 {
 public:
   Light(GLenum lightSrc, float offset, float lamp_z, int indice);
-
   ~Light();
-
   void Display();
-
   void Update(const double &deltaTime);
 
 private:
-  GLenum _lightSrc;
-
   GLfloat *_ambient, *_diffuse, *_specular, *_position;
-
-  GLfloat _c_falloff, _l_falloff, _q_falloff;
+  GLenum _lightSrc;
 };
--- gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
+++ gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/MyScene.cpp
@@ -22,27 +22,9 @@ void MyScene::Initialise()
 
 	float lamp_z = wall_z / 2.f;
 	float offset = wall_w / 4.f;
-	// Light A (red)
-
-	// static GLfloat ambient_A[] = {0.5f, 0.0f, 0.0f, 1.0f};
-	// static GLfloat diffuse_A[] = {0.9f, 0.0f, 0.0f, 1.0f};
-	// static GLfloat specular_A[] = {0.0f, 0.0f, 0.0f, 1.0f};
-	// static GLfloat position_A[] = {-offset, 0.0f, lamp_z, 1.0f};
-
-	// Light *l_A = new Light(GL_LIGHT0, ambient_A, diffuse_A, specular_A, position_A);
-
-	// // Light B (blue)
-
-	// static GLfloat ambient_B[] = {0.0f, 0.0f, 0.5f, 1.0f};
-	// static GLfloat diffuse_B[] = {0.0f, 0.0f, 0.9f, 1.0f};
-	// static GLfloat specular_B[] = {0.0f, 0.0f, 0.0f, 1.0f};
-	// static GLfloat position_B[] = {offset, 0.0f, lamp_z, 1.0f};
-
-	// Light *l_B = new Light(GL_LIGHT1, ambient_B, diffuse_B, specular_B, position_B);
-
 	AddObjectToScene(w);
-	AddObjectToScene(new Light(GL_LIGHT0, offset, lamp_z, 0));
-	AddObjectToScene(new Light(GL_LIGHT1, -offset, lamp_z, 2));
+	AddObjectToScene(new Light(GL_LIGHT0, offset, lamp_z, 2));
+	AddObjectToScene(new Light(GL_LIGHT1, -offset, lamp_z, 0));
 }
 
 void MyScene::Projection()
