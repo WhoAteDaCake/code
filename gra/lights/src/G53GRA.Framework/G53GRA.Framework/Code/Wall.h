222829e61de9a2dae26e4dc7d1690630ba7cd948
WhoAteDaCake <doitsimplefy@gmail.com>
2019-03-12 18:15:03 +0000

    initital state

---
+++ gra/lights/src/G53GRA.Framework/G53GRA.Framework/Code/Wall.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "DisplayableObject.h"
+
+class Wall : public DisplayableObject
+{
+public:
+  Wall();
+  ~Wall(){};
+
+  void Display();
+
+private:
+  GLfloat *_mat_ambient, *_mat_diffuse, *_mat_specular, _mat_shininess;
+};
