8e2dde4a4117e78e358d3de7f43179bfe4acba49
WhoAteDaCake <doitsimplefy@gmail.com>
2019-03-29 12:56:06 +0000

    update cell registry

--- dia/cw2/G53DIA Tanker MultiAgent/src/uk/ac/nott/cs/g53dia/multiagent/Agent.java
+++ dia/cw2/G53DIA Tanker MultiAgent/src/uk/ac/nott/cs/g53dia/multiagent/Agent.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 import uk.ac.nott.cs.g53dia.multilibrary.Action;
 import uk.ac.nott.cs.g53dia.multilibrary.Cell;
+import uk.ac.nott.cs.g53dia.multilibrary.EmptyCell;
+import uk.ac.nott.cs.g53dia.multilibrary.FuelPump;
 import uk.ac.nott.cs.g53dia.multilibrary.MoveAction;
+import uk.ac.nott.cs.g53dia.multilibrary.Station;
 import uk.ac.nott.cs.g53dia.multilibrary.Tanker;
 
 public class Agent extends Tanker {
@@ -24,8 +27,34 @@ public class Agent extends Tanker {
 		return new MoveAction(direction);
 	}
 	
+	/*
+	 * Find all cells nearby that are special (well/station/pump) TODO: in the
+	 * future probably don't need to run it every single move
+	 */
+	private void analyseView(Cell[][] view) {
+		// Make sure pump is loaded to allow for distance calculations
+		for (int x = 0; x < view.length; x += 1) {
+			for (int y = 0; y < view[x].length; y += 1) {
+				Cell current = view[x][y];
+				CellType type;
+
+				if (current instanceof EmptyCell) {
+					continue;
+				} else if (current instanceof Station) {
+					type = CellType.STATION;
+				} else if (current instanceof FuelPump) {
+					type = CellType.PUMP;
+				} else {
+					type = CellType.WELL;
+				}
+				m.w.saveGridCell(coords, current, type, x, y);
+			}
+		}
+	}
+	
 	@Override
 	public Action senseAndAct(Cell[][] view, boolean actionFailed, long timestep) {
+		analyseView(view);
 		// TODO Auto-generated method stub
 		return registeredMove(r.nextInt(8));
 	}
