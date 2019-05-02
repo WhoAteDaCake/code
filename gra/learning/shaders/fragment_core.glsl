0b6b864b801b546b84794ca98c39f351a94c3efa
WhoAteDaCake <doitsimplefy@gmail.com>
2019-05-02 15:32:05 +0100

    remove color mashing

--- gra/learning/shaders/fragment_core.glsl
+++ gra/learning/shaders/fragment_core.glsl
@@ -11,6 +11,6 @@ uniform sampler2D texture0;
 void main()
 {
 	// fs_color = vec4(vs_color, 1.f);
-	fs_color=texture(texture0,vs_texcoord)*vec4(vs_color,1.f);
+	fs_color=texture(texture0,vs_texcoord);//*vec4(vs_color,1.f);
 	// fs_color = texture(texture0, vs_texcoord) * texture(texture0, vs_texcoord) * vec4(vs_color, 1.f);
 }
