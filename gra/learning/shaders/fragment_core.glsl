9af260709b4a54d471c6b19387a2b98469f6c5c7
WhoAteDaCake <doitsimplefy@gmail.com>
2019-05-05 13:08:00 +0100

    Clean up fragment shader functions

--- gra/learning/shaders/fragment_core.glsl
+++ gra/learning/shaders/fragment_core.glsl
@@ -22,26 +22,34 @@ uniform sampler2D mat_specular_tex;
 uniform vec3 light_pos0;
 uniform vec3 camera_pos;
 
-void main()
-{
-	// Ambient (natural light)
-	vec3 ambient=material.ambient;
-	// Diffuse
-	vec3 diffuse_color=vec3(1.f,1.f,1.f);
-	vec3 pos_to_light=normalize(vs_position-light_pos0);
-	float dmod=clamp(dot(pos_to_light,vs_normal),0.f,1.f);
+vec4 get_ambient(Material material) {
+	return vec4(material.ambient, 1.f);
+}
+
+vec4 get_diffuse(Material material, vec3 position, vec3 normal, vec3 light_pos) {
+	vec3 pos_to_light=normalize(position-light_pos);
+	float dmod=clamp(dot(pos_to_light,normal),0.f,1.f);
 	vec3 diffuse=material.diffuse*dmod;
-	
-	// Specular
-	vec3 light_to_pos=normalize(light_pos0-vs_position);
-	vec3 reflect_dir=normalize(reflect(light_to_pos,normalize(vs_normal)));
-	vec3 pos_to_view=normalize(vs_position-camera_pos);
+	return vec4(diffuse, 1.f);
+}
+
+vec4 get_specular(Material material, vec3 position, vec3 normal, vec3 light_pos, vec3 camera_pos) {
+	vec3 light_to_pos=normalize(light_pos-position);
+	vec3 reflect_dir=normalize(reflect(light_to_pos,normalize(normal)));
+	vec3 pos_to_view=normalize(position-camera_pos);
 	float specular_const=pow(max(dot(pos_to_view,reflect_dir),0),30);
 	vec3 specular=material.specular*specular_const;
-	// Attenuation
-	
+	return vec4(specular, 1.f);
+}
+
+void main()
+{
+
 	// Output
-	vec4 light_final=vec4(ambient,1.f)+vec4(diffuse,1.f)+vec4(specular,1.f);
-	fs_color=texture(mat_diffuse_tex,vs_texcoord) * vec4(vs_color,1.f)*light_final;
+	vec4 light_final=
+		get_ambient(material)+
+		get_diffuse(material, vs_position, vs_normal, light_pos0)+
+		get_specular(material, vs_position, vs_normal, light_pos0, camera_pos);
+	fs_color=texture(mat_diffuse_tex,vs_texcoord)* vec4(vs_color,1.f)*light_final;
 	// fs_color=texture(texture0,vs_texcoord)*light_final;
 }
