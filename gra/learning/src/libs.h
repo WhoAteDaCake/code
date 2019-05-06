cf36e79e15e1102367e7b0b8c5fb86de593ac797
WhoAteDaCake <doitsimplefy@gmail.com>
2019-05-06 18:22:05 +0100

    fix libs imports

--- gra/learning/src/libs.h
+++ gra/learning/src/libs.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#ifndef LIBS_H
+#define LIBS_H
 
 #include <iostream>
 #include <fstream>
@@ -7,3 +11,10 @@
 #include "Mesh.h"
 #include "Shapes.h"
 #include "Engine.h"
+
+#endif // DEBUG
+
+// #ifndef TINY_LOADER_H
+// #define TINY_LOADER_H
+
+// #endif // !1TINY_LOADER_H
